1da787742155709244e4e8156216b5d3b9b9d8d7
Ferhat Sobay <ferhat.sobay@yahoo.com.tr>
2013-02-05 14:56:17 +0200

    Date Histogram

--- src/main/java/io/searchbox/core/search/facet/DateHistogramFacet.java
+++ src/main/java/io/searchbox/core/search/facet/DateHistogramFacet.java
@@ -1,7 +1,51 @@
 package io.searchbox.core.search.facet;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author ferhat
  */
 public class DateHistogramFacet {
+    public static final String TYPE = "date_histogram";
+
+    private String name;
+    private List<DateHistogram> dateHistograms;
+
+    public DateHistogramFacet(String name, Map dateHistogramFacet) {
+        this.name = name;
+        dateHistograms = new ArrayList<DateHistogram>();
+        for (Map term : (List<Map>) dateHistogramFacet.get("entries")) {
+            DateHistogram histogram = new DateHistogram(((Double) term.get("time")).longValue(), ((Double) term.get("count")).longValue());
+            dateHistograms.add(histogram);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<DateHistogram> getDateHistograms() {
+        return dateHistograms;
+    }
+
+    class DateHistogram {
+
+        private Long time;
+        private Long count;
+
+        DateHistogram(Long time, Long count) {
+            this.time = time;
+            this.count = count;
+        }
+
+        public Long getTime() {
+            return time;
+        }
+
+        public Long getCount() {
+            return count;
+        }
+    }
 }
