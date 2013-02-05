1ff961da2eaeee61730a391bb2367933bbe1b36e
Ferhat Sobay <ferhat.sobay@yahoo.com.tr>
2013-02-05 15:36:55 +0200

    Filter Facet

--- src/main/java/io/searchbox/core/search/facet/FilterFacet.java
+++ src/main/java/io/searchbox/core/search/facet/FilterFacet.java
@@ -1,7 +1,26 @@
 package io.searchbox.core.search.facet;
 
+import java.util.Map;
+
 /**
  * @author ferhat
  */
 public class FilterFacet {
+    public static final String TYPE = "filter";
+
+    private String name;
+    private Long count;
+
+    public FilterFacet(String name, Map filterFacet) {
+        this.name = name;
+        this.count = ((Double) filterFacet.get("count")).longValue();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getCount() {
+        return count;
+    }
 }
