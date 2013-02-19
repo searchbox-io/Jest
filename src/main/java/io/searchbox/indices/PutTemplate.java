890c315bdf3483141a1a8cd2b24ff49f9e2278e5
asierdelpozo <asierdelpozo@gmail.com>
2013-02-19 08:30:21 -0500

    Template management
    GetTemplate and PutTemplate action, with a PutTemplateIntegrationTest.
    Based on PutMapping/GetMapping and Elastic Search documentation

---
+++ src/main/java/io/searchbox/indices/PutTemplate.java
@@ -0,0 +1,29 @@
+package io.searchbox.indices;
+
+import io.searchbox.AbstractAction;
+import io.searchbox.Action;
+
+/**
+ * @author asierdelpozo
+ */
+
+public class PutTemplate extends AbstractAction implements Action {
+
+    public PutTemplate(String name, Object source) {
+        setURI(buildPutURI(name));
+        setData(source);
+        setRestMethodName("PUT");
+    }
+
+    private String buildPutURI(String name) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("_template").append("/").append(name);
+        return sb.toString();
+    }
+
+    @Override
+    public String getName() {
+        return "PUT_TEMPLATE";
+    }
+}
+
