3da1f58e486e6f11616f336a7b4ac62f63c76dc6
Cihat Keser <info@cihatkeser.com>
2014-07-20 00:57:33 +0300

    updated Refresh integration test to dynamically calculate number of expected refreshes depending on shard count.

--- jest/src/test/java/io/searchbox/indices/RefreshIntegrationTest.java
+++ jest/src/test/java/io/searchbox/indices/RefreshIntegrationTest.java
@@ -3,6 +3,7 @@ package io.searchbox.indices;
 import io.searchbox.client.JestResult;
 import io.searchbox.common.AbstractIntegrationTest;
 import org.elasticsearch.action.ActionFuture;
+import org.elasticsearch.action.admin.indices.stats.IndexStats;
 import org.elasticsearch.action.admin.indices.stats.IndicesStatsRequest;
 import org.elasticsearch.action.admin.indices.stats.IndicesStatsResponse;
 import org.elasticsearch.test.ElasticsearchIntegrationTest;
@@ -22,6 +23,7 @@ public class RefreshIntegrationTest extends AbstractIntegrationTest {
     @Test
     public void testFlushAll() throws InterruptedException, ExecutionException, TimeoutException, IOException {
         createIndex("i_flush_0", "i_flush_1", "i_flush_2");
+        ensureSearchable("i_flush_0", "i_flush_1", "i_flush_2");
 
         Refresh refresh = new Refresh.Builder().build();
         JestResult result = client.execute(refresh);
@@ -34,14 +36,20 @@ public class RefreshIntegrationTest extends AbstractIntegrationTest {
         assertNotNull(statsResponse);
 
         // Each index will have 1 refresh op counted at creation and 1 at the explicit refresh request
-        assertEquals(2, statsResponse.getIndex("i_flush_0").getTotal().getRefresh().getTotal());
-        assertEquals(2, statsResponse.getIndex("i_flush_1").getTotal().getRefresh().getTotal());
-        assertEquals(2, statsResponse.getIndex("i_flush_2").getTotal().getRefresh().getTotal());
+        IndexStats stats0 = statsResponse.getIndex("i_flush_0");
+        assertEquals(2 * stats0.getShards().length, stats0.getTotal().getRefresh().getTotal());
+
+        IndexStats stats1 = statsResponse.getIndex("i_flush_1");
+        assertEquals(2 * stats1.getShards().length, stats1.getTotal().getRefresh().getTotal());
+
+        IndexStats stats2 = statsResponse.getIndex("i_flush_2");
+        assertEquals(2 * stats2.getShards().length, stats2.getTotal().getRefresh().getTotal());
     }
 
     @Test
     public void testFlushSpecificIndices() throws InterruptedException, ExecutionException, TimeoutException, IOException {
         createIndex("i_flush_4", "i_flush_5", "i_flush_6");
+        ensureSearchable("i_flush_4", "i_flush_5", "i_flush_6");
 
         Refresh refresh = new Refresh.Builder().addIndex("i_flush_4").addIndex("i_flush_6").build();
         JestResult result = client.execute(refresh);
@@ -54,9 +62,14 @@ public class RefreshIntegrationTest extends AbstractIntegrationTest {
         assertNotNull(statsResponse);
 
         // Each index will have 1 refresh op counted at creation and 1 at the explicit refresh request
-        assertEquals(2, statsResponse.getIndex("i_flush_4").getTotal().getRefresh().getTotal());
-        assertEquals(1, statsResponse.getIndex("i_flush_5").getTotal().getRefresh().getTotal());
-        assertEquals(2, statsResponse.getIndex("i_flush_6").getTotal().getRefresh().getTotal());
+        IndexStats stats4 = statsResponse.getIndex("i_flush_4");
+        assertEquals(2 * stats4.getShards().length, stats4.getTotal().getRefresh().getTotal());
+
+        IndexStats stats5 = statsResponse.getIndex("i_flush_5");
+        assertEquals(1 * stats5.getShards().length, stats5.getTotal().getRefresh().getTotal());
+
+        IndexStats stats6 = statsResponse.getIndex("i_flush_6");
+        assertEquals(2 * stats6.getShards().length, stats6.getTotal().getRefresh().getTotal());
     }
 
 }
