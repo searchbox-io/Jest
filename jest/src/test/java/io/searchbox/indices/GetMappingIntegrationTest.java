67538beac429e6dfe0fe587e67eb30882419a23d
kramer <info@ckeser.com>
2014-10-21 16:03:28 +0200

    testfix: added assertion messages and retry conditions for get-mapping-integration-test

--- jest/src/test/java/io/searchbox/indices/GetMappingIntegrationTest.java
+++ jest/src/test/java/io/searchbox/indices/GetMappingIntegrationTest.java
@@ -22,7 +22,7 @@ public class GetMappingIntegrationTest extends AbstractIntegrationTest {
     private static final String INDEX_2_NAME = "video";
 
     @Test
-    public void testWithoutParameters() throws IOException {
+    public void testWithoutParameters() throws IOException, InterruptedException {
         createIndex(INDEX_1_NAME, INDEX_2_NAME);
 
         client().admin().indices().putMapping(new PutMappingRequest(INDEX_1_NAME)
@@ -34,20 +34,35 @@ public class GetMappingIntegrationTest extends AbstractIntegrationTest {
 
         GetMapping getMapping = new GetMapping.Builder().build();
         JestResult result = client.execute(getMapping);
-        assertNotNull(result);
+        assertTrue(result.isSucceeded());
 
-        JsonObject mappings = result.getJsonObject().getAsJsonObject(INDEX_2_NAME).getAsJsonObject("mappings");
-        assertEquals(1, mappings.entrySet().size());
-        assertNotNull(mappings.get("_default_"));
+        JsonObject resultJson;
+        JsonObject index1Object;
+        JsonObject index2Object;
+        int retries = 0;
+        do {
+            resultJson = result.getJsonObject();
+            index1Object = resultJson.getAsJsonObject(INDEX_1_NAME);
+            index2Object = resultJson.getAsJsonObject(INDEX_2_NAME);
+            if(retries > 0) Thread.sleep(1000);
+            retries++;
+        } while ((index1Object == null || index2Object == null) && retries < 3);
 
-        mappings = result.getJsonObject().getAsJsonObject(INDEX_1_NAME).getAsJsonObject("mappings");
-        assertEquals(2, mappings.entrySet().size());
-        assertNotNull(mappings.get("_default_"));
-        assertNotNull(mappings.get("science-fiction"));
+        assertNotNull("GetMapping response JSON should include the index " + INDEX_1_NAME, index1Object);
+        assertNotNull("GetMapping response JSON should include the index " + INDEX_2_NAME, index2Object);
+
+        JsonObject index1Mappings = index2Object.getAsJsonObject("mappings");
+        assertEquals(1, index1Mappings.entrySet().size());
+        assertNotNull(index1Mappings.get("_default_"));
+
+        JsonObject index2Mappings = index1Object.getAsJsonObject("mappings");
+        assertEquals(2, index2Mappings.entrySet().size());
+        assertNotNull(index2Mappings.get("_default_"));
+        assertNotNull(index2Mappings.get("science-fiction"));
     }
 
     @Test
-    public void testWithSingleIndex() throws IOException {
+    public void testWithSingleIndex() throws IOException, InterruptedException {
         createIndex(INDEX_1_NAME, INDEX_2_NAME);
 
         client().admin().indices().putMapping(new PutMappingRequest(INDEX_1_NAME)
@@ -59,15 +74,26 @@ public class GetMappingIntegrationTest extends AbstractIntegrationTest {
 
         Action getMapping = new GetMapping.Builder().addIndex(INDEX_2_NAME).build();
         JestResult result = client.execute(getMapping);
-        assertNotNull(result);
+        assertTrue(result.isSucceeded());
 
-        JsonObject mappings = result.getJsonObject().getAsJsonObject(INDEX_2_NAME).getAsJsonObject("mappings");
+        JsonObject resultJson;
+        JsonObject index2Object;
+        int retries = 0;
+        do {
+            resultJson = result.getJsonObject();
+            index2Object = resultJson.getAsJsonObject(INDEX_2_NAME);
+            if(retries > 0) Thread.sleep(1000);
+            retries++;
+        } while (index2Object == null && retries < 3);
+        assertNotNull("GetMapping response JSON should include the index " + INDEX_2_NAME, index2Object);
+
+        JsonObject mappings = index2Object.getAsJsonObject("mappings");
         assertEquals(1, mappings.entrySet().size());
         assertNotNull(mappings.get("_default_"));
     }
 
     @Test
-    public void testWithMultipleIndices() throws IOException {
+    public void testWithMultipleIndices() throws IOException, InterruptedException {
         createIndex(INDEX_1_NAME, INDEX_2_NAME, "irrelevant");
 
         client().admin().indices().putMapping(new PutMappingRequest(INDEX_1_NAME)
@@ -79,17 +105,30 @@ public class GetMappingIntegrationTest extends AbstractIntegrationTest {
 
         Action getMapping = new GetMapping.Builder().addIndex(INDEX_2_NAME).addIndex(INDEX_1_NAME).build();
         JestResult result = client.execute(getMapping);
-        assertNotNull(result);
+        assertTrue(result.isSucceeded());
 
+        JsonObject resultJson;
+        JsonObject index1Object;
+        JsonObject index2Object;
+        int retries = 0;
+        do {
+            resultJson = result.getJsonObject();
+            index1Object = resultJson.getAsJsonObject(INDEX_1_NAME);
+            index2Object = resultJson.getAsJsonObject(INDEX_2_NAME);
+            if(retries > 0) Thread.sleep(1000);
+            retries++;
+        } while ((index1Object == null || index2Object == null) && retries < 3);
+        assertNotNull("GetMapping response JSON should include the index " + INDEX_1_NAME, index1Object);
+        assertNotNull("GetMapping response JSON should include the index " + INDEX_2_NAME, index2Object);
 
-        JsonObject mappings = result.getJsonObject().getAsJsonObject(INDEX_2_NAME).getAsJsonObject("mappings");
-        assertEquals(1, mappings.entrySet().size());
-        assertNotNull(mappings.get("_default_"));
+        JsonObject index2Mappings = index2Object.getAsJsonObject("mappings");
+        assertEquals(1, index2Mappings.entrySet().size());
+        assertNotNull(index2Mappings.get("_default_"));
 
-        mappings = result.getJsonObject().getAsJsonObject(INDEX_1_NAME).getAsJsonObject("mappings");
-        assertEquals(2, mappings.entrySet().size());
-        assertNotNull(mappings.get("_default_"));
-        assertNotNull(mappings.get("science-fiction"));
+        JsonObject index1Mappings = index1Object.getAsJsonObject("mappings");
+        assertEquals(2, index1Mappings.entrySet().size());
+        assertNotNull(index1Mappings.get("_default_"));
+        assertNotNull(index1Mappings.get("science-fiction"));
     }
 
     /**
