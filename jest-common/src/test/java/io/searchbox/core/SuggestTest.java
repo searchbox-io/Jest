5ac86e4cded386759ca1e690dfc8b8aa4bb4b123
Fernandez Ludovic <ludovic.fernandez@zenika.com>
2014-10-22 21:31:39 +0200

    Adding the simplest Suggest Action

---
+++ jest-common/src/test/java/io/searchbox/core/SuggestTest.java
@@ -0,0 +1,67 @@
+package io.searchbox.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import io.searchbox.action.Action;
+import io.searchbox.client.JestResult;
+
+import org.junit.Test;
+
+public class SuggestTest {
+
+    @Test
+    public void getURIWithoutIndexAndType() {
+        Action<JestResult> suggest = new Suggest.Builder("").build();
+        assertEquals("_all/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithOnlyOneIndex() {
+        Action<JestResult> suggest = new Suggest.Builder("").addIndex("twitter").build();
+        assertEquals("twitter/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithOneIndexAndOneType() {
+        Action<JestResult> suggest = new Suggest.Builder("").addIndex("twitter").addType("tweet").build();
+        assertEquals("twitter/tweet/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithOnlyOneType() {
+        Action<JestResult> suggest = new Suggest.Builder("").addType("tweet").build();
+        assertEquals("_all/tweet/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithOnlyMultipleIndex() {
+        Action<JestResult> suggest = new Suggest.Builder("").addIndex("twitter").addIndex("suggestbox").build();
+        assertEquals("twitter%2Csuggestbox/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithOnlyMultipleType() {
+        Action<JestResult> suggest = new Suggest.Builder("").addType("tweet").addType("jest").build();
+        assertEquals("_all/tweet%2Cjest/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getURIWithMultipleIndexAndTypes() {
+        Action<JestResult> suggest = new Suggest.Builder("")
+            .addIndex("twitter")
+            .addIndex("suggestbox")
+            .addType("tweet")
+            .addType("jest")
+            .build();
+        assertEquals("twitter%2Csuggestbox/tweet%2Cjest/_suggest", suggest.getURI());
+    }
+
+    @Test
+    public void getDataSimple() {
+        String query = "{\"elementSuggestion\":{\"text\":\"courgette\",\"completion\":{\"field\":\"element.completion\",\"size\":10,\"fuzzy\":{}}}}";
+        Action<JestResult> suggest = new Suggest.Builder(query).build();
+
+        assertNotNull("data", suggest.getData(null));
+        assertEquals(query, suggest.getData(null).toString());
+    }
+}
