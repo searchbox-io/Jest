369a4b8f27b5b9b1b55832230a8bf0a56b5a545c
Cihat Keser <info@cihatkeser.com>
2014-11-17 21:09:57 +0100

    added 'routing' and 'source' fields to MultiGet request helper class Doc - fixes #161

--- jest/src/test/java/io/searchbox/core/MultiGetIntegrationTest.java
+++ jest/src/test/java/io/searchbox/core/MultiGetIntegrationTest.java
@@ -1,6 +1,7 @@
 package io.searchbox.core;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.searchbox.action.Action;
 import io.searchbox.client.JestResult;
 import io.searchbox.common.AbstractIntegrationTest;
@@ -25,9 +26,50 @@ public class MultiGetIntegrationTest extends AbstractIntegrationTest {
 
     @Before
     public void setup() throws IOException {
-        client().index(new IndexRequest(TEST_INDEX, TEST_TYPE, "1").source("{\"text\":\"pumpkin\"}")).actionGet();
+        client().index(new IndexRequest(TEST_INDEX, TEST_TYPE, "1").source("{\"text\":\"pumpkin\", \"author\":\"anon\"}")).actionGet();
         client().index(new IndexRequest(TEST_INDEX, TEST_TYPE, "2").source("{\"text\":\"spice\"}")).actionGet();
         client().index(new IndexRequest(TEST_INDEX, TEST_TYPE, "3").source("{\"text\":\"latte\"}")).actionGet();
+    }
+
+    @Test
+    public void getWithPartialSource() throws IOException {
+        Doc doc1 = new Doc(TEST_INDEX, TEST_TYPE, "1");
+        doc1.setSource("author");
+
+        Action action = new MultiGet.Builder.ByDoc(doc1).build();
+        JestResult result = client.execute(action);
+        assertTrue(result.getErrorMessage(), result.isSucceeded());
+        result.getJsonObject().getAsJsonArray("docs");
+
+        JsonArray actualDocs = result.getJsonObject().getAsJsonArray("docs");
+        assertEquals("Number of docs in response should match the number of docs in requests.", 1, actualDocs.size());
+
+        JsonObject actualDoc1 = actualDocs.get(0).getAsJsonObject();
+        assertTrue("Document 1 is indexed and should have been found by the MultiGet request.",
+                actualDoc1.getAsJsonPrimitive("found").getAsBoolean());
+        JsonObject actualSource = actualDoc1.getAsJsonObject("_source");
+        assertNotNull("Response doc should have source", actualSource);
+        assertNull("Response doc source should not have unrequested text field", actualSource.get("text"));
+        assertNotNull("Response doc source should have requested author field", actualSource.get("author"));
+    }
+
+    @Test
+    public void getWithoutSource() throws IOException {
+        Doc doc1 = new Doc(TEST_INDEX, TEST_TYPE, "1");
+        doc1.setSource(Boolean.FALSE);
+
+        Action action = new MultiGet.Builder.ByDoc(doc1).build();
+        JestResult result = client.execute(action);
+        assertTrue(result.getErrorMessage(), result.isSucceeded());
+        result.getJsonObject().getAsJsonArray("docs");
+
+        JsonArray actualDocs = result.getJsonObject().getAsJsonArray("docs");
+        assertEquals("Number of docs in response should match the number of docs in requests.", 1, actualDocs.size());
+
+        JsonObject actualDoc1 = actualDocs.get(0).getAsJsonObject();
+        assertTrue("Document 1 is indexed and should have been found by the MultiGet request.",
+                actualDoc1.getAsJsonPrimitive("found").getAsBoolean());
+        assertNull("Response doc should not have source", actualDoc1.get("_source"));
     }
 
     @Test
