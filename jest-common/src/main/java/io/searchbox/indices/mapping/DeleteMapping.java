7c994ba8e218a8db2aacf28e7ab59297e778a4b4
François Thareau <francois.thareau@gmail.com>
2015-03-13 09:01:49 +0100

    #186 Implementation of DeleteMapping

--- jest-common/src/main/java/io/searchbox/indices/mapping/DeleteMapping.java
+++ jest-common/src/main/java/io/searchbox/indices/mapping/DeleteMapping.java
@@ -1,9 +1,46 @@
 package io.searchbox.indices.mapping;
 
+
+import io.searchbox.action.GenericResultAbstractAction;
+
 /**
  * @author Dogukan Sonmez
+ * @author François Thareau
  */
+public class DeleteMapping extends GenericResultAbstractAction {
 
+    public DeleteMapping(Builder builder) {
+        super(builder);
 
-public class DeleteMapping {
+        this.indexName = builder.index;
+        this.typeName = builder.type;
+        setURI(buildURI());
+    }
+
+    @Override
+    protected String buildURI() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.buildURI()).append("/_mapping");
+        return sb.toString();
+    }
+
+    @Override
+    public String getRestMethodName() {
+        return "DELETE";
+    }
+
+    public static class Builder extends GenericResultAbstractAction.Builder<DeleteMapping, Builder> {
+        private String index;
+        private String type;
+
+        public Builder(String index, String type) {
+            this.index = index;
+            this.type = type;
+        }
+
+        @Override
+        public DeleteMapping build() {
+            return new DeleteMapping(this);
+        }
+    }
 }
