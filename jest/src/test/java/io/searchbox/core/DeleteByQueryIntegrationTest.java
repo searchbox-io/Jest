959b054d37fb2e04dfd95495ebe4bdb0a9dbc7e9
andrejserafim <andrejserafim@gmail.com>
2015-10-17 16:28:10 +0100

    delete by query is now a plugin
    https://www.elastic.co/guide/en/elasticsearch/plugins/2.0/plugins-delete-by-query.html
    
    solving jar hell with junit, mockito and hamcrest.
    
    upgrading to es2, rc1.

--- jest/src/test/java/io/searchbox/core/DeleteByQueryIntegrationTest.java
+++ jest/src/test/java/io/searchbox/core/DeleteByQueryIntegrationTest.java
@@ -1,11 +1,18 @@
 package io.searchbox.core;
 
+import com.google.common.base.Predicate;
 import io.searchbox.client.JestResult;
 import io.searchbox.common.AbstractIntegrationTest;
+import org.elasticsearch.index.engine.Engine;
+import org.elasticsearch.plugin.deletebyquery.DeleteByQueryPlugin;
+import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * @author Dogukan Sonmez
@@ -14,25 +21,52 @@ import java.io.IOException;
 public class DeleteByQueryIntegrationTest extends AbstractIntegrationTest {
 
     @Test
-    public void delete() throws IOException {
-        String query = "{\n" +
-                "    \"query\" : {\n" +
-                "        \"term\" : { \"user\" : \"kimchy\" }\n" +
+    public void delete() throws IOException, InterruptedException {
+        final String query = "{\n" +
+                "    \"query\": {\n" +
+                "        \"term\": { \"user\" : \"kimchy\" }\n" +
                 "    }\n" +
                 "}";
-        client.execute(new Index.Builder("\"user\":\"kimchy\"").index("twitter").type("tweet").id("1").build());
+        client.execute(new Index.Builder("{\"user\":\"kimchy\"}").index("twitter").type("tweet").id("1").build());
+
+        waitForDocumentToBeIndexed(query);
+
+        Thread.sleep(1000);
+
         DeleteByQuery deleteByQuery = new DeleteByQuery.Builder(query)
                 .addIndex("twitter")
                 .addType("tweet")
                 .build();
 
         JestResult result = client.execute(deleteByQuery);
-        assertTrue(result.getErrorMessage(), result.isSucceeded());;
+        assertTrue(result.getErrorMessage(), result.isSucceeded());
 
         assertEquals(
                 0,
-                result.getJsonObject().getAsJsonObject("_indices").getAsJsonObject("twitter").getAsJsonObject("_shards").get("failed").getAsInt()
+                result.getJsonObject().getAsJsonObject("_indices").getAsJsonObject("twitter").get("failed").getAsInt()
         );
+        assertEquals(
+                1,
+                result.getJsonObject().getAsJsonObject("_indices").getAsJsonObject("twitter").get("deleted").getAsInt()
+        );
+    }
+
+    private void waitForDocumentToBeIndexed(final String query) throws InterruptedException {
+        awaitBusy(new Predicate<Object>() {
+            @Override
+            public boolean apply(Object input) {
+                try {
+                    SearchResult searchResult = client.execute(new Search.Builder(query)
+                            .addIndex("twitter")
+                            .addType("tweet")
+                            .build());
+                    System.out.println(searchResult.getJsonString());
+                    return null != searchResult.getFirstHit(Map.class);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 
 }
