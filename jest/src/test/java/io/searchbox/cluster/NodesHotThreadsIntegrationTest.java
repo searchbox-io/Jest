46bd54267795217b4e5463580c9b88dfabb87a18
andrejserafim <andrejserafim@gmail.com>
2015-10-18 13:28:21 +0100

    trivial commit to re-trigger travis

--- jest/src/test/java/io/searchbox/cluster/NodesHotThreadsIntegrationTest.java
+++ jest/src/test/java/io/searchbox/cluster/NodesHotThreadsIntegrationTest.java
@@ -56,6 +56,7 @@ public class NodesHotThreadsIntegrationTest extends AbstractIntegrationTest {
     }
 
     private void assertNodePresent(JestResult result, String node) {
+        /// axk
         assertTrue(result.getJsonString().contains("::: {" + node + "}{"));
     }
 
