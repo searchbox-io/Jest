dc048bde6e07f13a112b240ec621b9e915704996
jeehong Lee <jeehong.lee@linecorp.com>
2015-11-18 16:59:41 +0900

    1. print round robin serverList; 2. refactoring testCase code, testGetElasticSearchServer

--- jest-common/src/test/java/io/searchbox/client/AbstractJestClientTest.java
+++ jest-common/src/test/java/io/searchbox/client/AbstractJestClientTest.java
@@ -1,10 +1,12 @@
 package io.searchbox.client;
 
 import io.searchbox.action.Action;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -39,21 +41,54 @@ public class AbstractJestClientTest {
     @Test
     public void testGetElasticSearchServer() throws Exception {
         LinkedHashSet<String> set = new LinkedHashSet<String>();
-        set.add("http://localhost:9200");
-        set.add("http://localhost:9300");
-        set.add("http://localhost:9400");
+
+        String node1 = "http://localhost:9200";
+        String node2 = "http://localhost:9300";
+        String node3 = "http://localhost:9400";
+
+        set.add(node1);
+        set.add(node2);
+        set.add(node3);
         client.setServers(set);
 
         Set<String> serverList = new HashSet<String>();
-
         for (int i = 0; i < 3; i++) {
             serverList.add(client.getNextServer());
         }
 
         assertEquals("round robin does not work", 3, serverList.size());
 
-        assertTrue(set.contains("http://localhost:9200"));
-        assertTrue(set.contains("http://localhost:9300"));
-        assertTrue(set.contains("http://localhost:9400"));
+        assertTrue(set.contains(node1));
+        assertTrue(set.contains(node2));
+        assertTrue(set.contains(node3));
     }
+
+    @Test
+    public void testServerPoolReferencePair() throws Exception {
+        LinkedHashSet<String> set = new LinkedHashSet<String>();
+
+        String node1 = "http://localhost:9200";
+        String node2 = "http://localhost:9300";
+        String node3 = "http://localhost:9400";
+
+        set.add(node1);
+        set.add(node2);
+        set.add(node3);
+        client.setServers(set);
+
+        Pair<Integer, Iterator<String>> serverPoolReference = client.getServerPoolReferencePair();
+
+        Set<String> serverList = new HashSet<String>();
+
+        for (int i = 0; i < serverPoolReference.getKey(); i++) {
+            serverList.add(serverPoolReference.getValue().next());
+        }
+
+        assertEquals("could not print serverList", 3, serverList.size());
+
+        assertTrue(set.contains(node1));
+        assertTrue(set.contains(node2));
+        assertTrue(set.contains(node3));
+    }
+
 }
