242f6403fa2e5429f5c5fb378df66d109e9fb6b5
Sebastian Hilbig <seh@heise.de>
2016-04-25 11:44:59 +0200

    Handles Elasticsearch metadata field "_version" in extractHit()

--- jest-common/src/main/java/io/searchbox/core/SearchResult.java
+++ jest-common/src/main/java/io/searchbox/core/SearchResult.java
@@ -98,6 +98,7 @@ public class SearchResult extends JestResult {
 
             if (source != null) {
                 JsonElement id = hitObject.get("_id");
+                JsonElement version = hitObject.get("_version");
                 String index = hitObject.get("_index").getAsString();
                 String type = hitObject.get("_type").getAsString();
 
@@ -113,6 +114,9 @@ public class SearchResult extends JestResult {
                 if (id != null) {
                     source = GsonUtils.deepCopy(source);
                     source.add(ES_METADATA_ID, id);
+                    if (version != null) {
+                        source.add(ES_METADATA_VERSION, version);
+                    }
                 }
                 hit = new Hit<T, K>(
                         sourceType,
