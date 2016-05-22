d222b975d262ab69a9f755cdd85eb46458739ef2
Cihat Keser <ckeser@gmail.com>
2016-05-22 13:49:15 +0200

    make extension point JestClientFactory.createPreemptiveAuthContext protected

--- jest/src/main/java/io/searchbox/client/JestClientFactory.java
+++ jest/src/main/java/io/searchbox/client/JestClientFactory.java
@@ -244,7 +244,7 @@ public class JestClientFactory {
     }
 
     // Extension point
-    private HttpClientContext createPreemptiveAuthContext(HttpHost targetHost) {
+    protected HttpClientContext createPreemptiveAuthContext(HttpHost targetHost) {
         HttpClientContext context = HttpClientContext.create();
         context.setCredentialsProvider(httpClientConfig.getCredentialsProvider());
         context.setAuthCache(createBasicAuthCache(targetHost));
