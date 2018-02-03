1117b0daa74d5a11761bd263ad2020db7bfc4dff
Michael Siepmann <misi1967@yahoo.de>
2018-02-03 17:53:56 +0100

    Fixed Tests.

--- jest-common/src/test/java/io/searchbox/core/CatAllocationBuilderTest.java
+++ jest-common/src/test/java/io/searchbox/core/CatAllocationBuilderTest.java
@@ -1,5 +1,6 @@
 package io.searchbox.core;
 
+import io.searchbox.client.config.ElasticsearchVersion;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,30 +16,30 @@ public class CatAllocationBuilderTest {
     @Test
     public void shouldGenerateValidUriWhenIndexNotGiven() {
         Cat cat = new Cat.AllocationBuilder().build();
-        assertEquals("_cat/allocation", cat.getURI());
+        assertEquals("_cat/allocation", cat.getURI(ElasticsearchVersion.UNKNOWN));
     }
 
     @Test
     public void shouldGenerateValidUriWhenSingleNodeGiven() {
         Cat cat = new Cat.AllocationBuilder().addNode("testNode").build();
-        assertEquals("_cat/allocation/testNode", cat.getURI());
+        assertEquals("_cat/allocation/testNode", cat.getURI(ElasticsearchVersion.UNKNOWN));
     }
 
     @Test
     public void shouldGenerateValidUriWhenNodesGiven() {
         Cat cat = new Cat.AllocationBuilder().addNode("testNode1").addNode("testNode2").build();
-        assertEquals("_cat/allocation/testNode1%2CtestNode2", cat.getURI());
+        assertEquals("_cat/allocation/testNode1%2CtestNode2", cat.getURI(ElasticsearchVersion.UNKNOWN));
     }
 
     @Test
     public void shouldGenerateValidUriWhenParameterGiven() {
         Cat cat = new Cat.AllocationBuilder().setParameter("v", "true").build();
-        assertEquals("_cat/allocation?v=true", cat.getURI());
+        assertEquals("_cat/allocation?v=true", cat.getURI(ElasticsearchVersion.UNKNOWN));
     }
 
     @Test
     public void shouldGenerateValidUriWhenHeadersParameterGiven() {
         Cat cat = new Cat.AllocationBuilder().setParameter("h", "shards,disk.indices,disk.used").build();
-        assertEquals("_cat/allocation?h=shards%2Cdisk.indices%2Cdisk.used", cat.getURI());
+        assertEquals("_cat/allocation?h=shards%2Cdisk.indices%2Cdisk.used", cat.getURI(ElasticsearchVersion.UNKNOWN));
     }
 }
