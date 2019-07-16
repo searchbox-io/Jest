b62c507346565c76d1c84eb7ff5a7826e9618b07
alessandro negrin <alessandro.negrin@gmail.com>
2019-07-16 21:25:38 +0200

    DocBuilder and ScriptBuilder for Update action (#638)

--- jest-common/src/main/java/io/searchbox/core/Update.java
+++ jest-common/src/main/java/io/searchbox/core/Update.java
@@ -1,5 +1,6 @@
 package io.searchbox.core;
 
+import com.google.gson.Gson;
 import io.searchbox.action.BulkableAction;
 import io.searchbox.action.SingleResultAbstractDocumentTargetedAction;
 import io.searchbox.client.config.ElasticsearchVersion;
@@ -11,9 +12,15 @@ import io.searchbox.params.Parameters;
  */
 public class Update extends SingleResultAbstractDocumentTargetedAction implements BulkableAction<DocumentResult> {
 
+    private enum Method {doc, script}
+
+    private static final String DOC_UPDATE_TEMPLATE = "{\"%s\": %s}";
+    private final Method method;
+
     protected Update(Builder builder) {
         super(builder);
         this.payload = builder.payload;
+        this.method = builder.method;
     }
 
     @Override
@@ -36,11 +43,28 @@ public class Update extends SingleResultAbstractDocumentTargetedAction implement
         return "ok";
     }
 
+    @Override
+    public String getData(Gson gson) {
+        String data = super.getData(gson);
+        if (method == null) {
+            return data;
+        } else {
+            return String.format(DOC_UPDATE_TEMPLATE, method.name(), data);
+        }
+    }
+
     public static class Builder extends SingleResultAbstractDocumentTargetedAction.Builder<Update, Builder> {
+
         private final Object payload;
+        private final Method method;
 
         public Builder(Object payload) {
+            this(payload, null);
+        }
+
+        private Builder(Object payload, Method method) {
             this.payload = payload;
+            this.method = method;
         }
 
         public Update build() {
@@ -49,9 +73,24 @@ public class Update extends SingleResultAbstractDocumentTargetedAction implement
     }
 
     public static class VersionBuilder extends Builder {
+
         public VersionBuilder(Object payload, Long version) {
             super(payload);
             this.setParameter(Parameters.VERSION, version);
+        }
+    }
+
+    public static class DocBuilder extends Builder {
+
+        public DocBuilder(Object payload) {
+            super(payload, Method.doc);
+        }
+    }
+
+    public static class ScriptBuilder extends Builder {
+
+        public ScriptBuilder(Object payload) {
+            super(payload, Method.script);
         }
     }
 }
